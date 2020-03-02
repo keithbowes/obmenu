c3aac3b714e44413b69d330ecc5773eecdb317fb
Keith Bowes <keithbowes@users.noreply.github.com>
2020-03-02 15:31:20 -0500

    Moved the window and about dialog to separate glade files

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ sys.path += [os.path.join(os.curdir, libdir)]
 
 setup(name='obMenu',
       version='1.0',
-      description='Openbox Menu Editor',
+      description='Openbox/Waybox Menu Editor',
       author='Manuel Colmenero',
       author_email='m.kolme@gmail.com',
       scripts=['obmenu', 'pipes/obm-xdg','pipes/obm-dir','pipes/obm-nav'],
       py_modules=['obxml'],
-      data_files=[(libdir, ['obmenu.glade', 'icons/mnu16.png','icons/mnu48.png'])]
+      data_files=[(libdir, ['about.glade', 'window.glade', 'icons/mnu16.png','icons/mnu48.png'])]
       )
